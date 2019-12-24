81129d66ef9f84d480d120ff304a6ebdc744e6b3
Abdi Suleman <abdirahman.suleman@target.com>
2019-12-24 16:46:54 -0600

    firebase added

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 var express = require("express");
+var chalk = require("chalk");
+var morgan = require("morgan");
+var path = require("path");
+const firebase = require("firebase");
+require("firebase/firestore");
+
 var app = express();
-var router = express.Router();
-var path = __dirname + "/views/";
 
-router.use(function(req, res, next) {
-  console.log("/" + req.method);
-  next();
+app.use(morgan("tiny"));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/views/", "/index.html"));
 });
-
-router.get("/", function(req, res) {
-  res.sendFile(path + "index.html");
-});
-
-app.use("/", router);
 
 app.listen(3000, function() {
-  console.log("Live at Port 3000");
+  console.log(`listening on port ${chalk.green(3000)}`);
 });
